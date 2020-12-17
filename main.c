cdd58750a4e8a2a59ced3d0697669c9e2677e58d
Meiline Monier <meiline.monier@yahoo.fr>
2020-12-17 16:06:52 +0100

    push

--- main.c
+++ main.c
@@ -3,11 +3,14 @@
 
 int	ft_fibonacci(int index)
 {
-	if (index < 0)
+//	static int a =0;
+//	a++;
+//	printf("count %d\n", a);
+/*	if (index < 0)
 		return (-1);
 	if (index == 0)
-		return (0);
-	else if (index == 1)
+		return (0);*/
+	if (index <= 1)
 		return (1);
 	else
 		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
